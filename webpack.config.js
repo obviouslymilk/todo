2f9b39f2faa82bb6f0b780071237c57ee980257e
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-08-16 11:13:18 +1000

    Remove clean on build

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     devtool: 'inline-source-map',
+    mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
     }
 }
