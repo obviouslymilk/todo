7f0dfd7bc13b28a595d31bcc60cd23dc6abc0a86
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-08-22 10:27:42 +1000

    Linting

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,41 +1,38 @@
-import Events from './events';
+import Events from './events'
 
-function updateEntry(name, date, comp) {
-    Events.emit('updateEntry', name, date, comp);
+function updateEntry (name, date, comp) {
+    Events.emit('updateEntry', name, date, comp)
 }
 
 export default class Display {
-    static projectsContainer = document.querySelector('#custom-projects');
-    static entriesContainer = document.querySelector('.entries');
-    static addProjectInput = document.querySelector('#project-textbox');
-    static addEntryInput = document.querySelector('#entry-textbox');
-    static title = document.querySelector('#display-title');
+    static projectsContainer = document.querySelector('#custom-projects')
+    static entriesContainer = document.querySelector('.entries')
+    static addProjectInput = document.querySelector('#project-textbox')
+    static addEntryInput = document.querySelector('#entry-textbox')
+    static title = document.querySelector('#display-title')
 
-
-    static init() {  
-          Display.updateInput();
-          Display.updateEvents();
+    static init () {
+        Display.updateInput()
+        Display.updateEvents()
     }
 
-    
-    static addProject(project) {
+    static addProject (project) {
         const p = `
         <div class="project" data-name='${project.name}'>
             <span class="material-symbols-rounded project-icon">list</span>
             <p class='project-button'>${project.name}</p> <span class="material-symbols-rounded delete-icon">delete</span>
         </div>
         `
-        Display.projectsContainer.insertAdjacentHTML("beforeend", p);
+        Display.projectsContainer.insertAdjacentHTML('beforeend', p)
 
-        const btn = document.querySelector(`.project[data-name='${project.name}'] > .delete-icon`);
-        btn.addEventListener('click', e => Events.emit('tryRemoveProject', e.target.parentElement.dataset.name));
-        const pb = document.querySelector(`.project[data-name='${project.name}'] > .project-button`);
-        pb.addEventListener('click', e => Events.emit('tryUpdateProject', e.target.parentElement.dataset.name));
+        const btn = document.querySelector(`.project[data-name='${project.name}'] > .delete-icon`)
+        btn.addEventListener('click', e => Events.emit('tryRemoveProject', e.target.parentElement.dataset.name))
+        const pb = document.querySelector(`.project[data-name='${project.name}'] > .project-button`)
+        pb.addEventListener('click', e => Events.emit('tryUpdateProject', e.target.parentElement.dataset.name))
     }
 
-
-    static addEntry(entry) {
-        if (!Display.entriesContainer.id) return;
+    static addEntry (entry) {
+        if (!Display.entriesContainer.id) return
         const e = `
         <div class="entry" data-name='${entry.name}'>
                 <input class='entry-check' type="checkbox" name="complete", ${entry.complete ? 'checked' : ''}>
@@ -47,82 +44,76 @@ export default class Display {
         </div> 
         `
 
-        Display.entriesContainer.insertAdjacentHTML("beforeend", e);
+        Display.entriesContainer.insertAdjacentHTML('beforeend', e)
 
-        const btn = document.querySelector(`.entry[data-name='${entry.name}'] > .delete-icon`);
-        btn.addEventListener('click', e => Events.emit('tryRemoveEntry', e.target.parentElement.dataset.name));
+        const btn = document.querySelector(`.entry[data-name='${entry.name}'] > .delete-icon`)
+        btn.addEventListener('click', e => Events.emit('tryRemoveEntry', e.target.parentElement.dataset.name))
 
-        const date = document.querySelector(`.entry[data-name='${entry.name}'] > .info > .date-input`);
-        const complete = document.querySelector(`.entry[data-name='${entry.name}'] > .entry-check`);
-        date.addEventListener('input', e => updateEntry(entry.name, date.value, complete.checked));
+        const date = document.querySelector(`.entry[data-name='${entry.name}'] > .info > .date-input`)
+        const complete = document.querySelector(`.entry[data-name='${entry.name}'] > .entry-check`)
+        date.addEventListener('input', e => updateEntry(entry.name, date.value, complete.checked))
         complete.addEventListener('change', e => updateEntry(entry.name, date.value, complete.checked))
     }
 
-
-    static removeProject(name) {
-        const p = document.querySelector(`.project[data-name="${name}"]`);
+    static removeProject (name) {
+        const p = document.querySelector(`.project[data-name="${name}"]`)
         if (p) {
-            p.remove();
-            console.log(`Project ${name} was removed.`);
+            p.remove()
+            console.log(`Project ${name} was removed.`)
         }
         if (name === Display.entriesContainer.id) {
-            Display.clearEntries();
-            Display.entriesContainer.removeAttribute('id');
-            Display.title.textContent = '';
+            Display.clearEntries()
+            Display.entriesContainer.removeAttribute('id')
+            Display.title.textContent = ''
         }
     }
 
-
-    static removeEntry(name) {
-        const e = document.querySelector(`.entry[data-name="${name}"]`);
+    static removeEntry (name) {
+        const e = document.querySelector(`.entry[data-name="${name}"]`)
         if (e) {
-            e.remove();
-        }    
-    }
- 
-
-    static clearEntries() {
-        Display.entriesContainer.replaceChildren();
+            e.remove()
+        }
     }
 
+    static clearEntries () {
+        Display.entriesContainer.replaceChildren()
+    }
 
-    static updateProject(project) {
-        Display.clearEntries();
+    static updateProject (project) {
+        Display.clearEntries()
         project.getEntries().forEach(e => {
-            Display.addEntry(e);
-        });
-        Display.entriesContainer.id = project.name;
-        Display.title.textContent = project.name;
+            Display.addEntry(e)
+        })
+        Display.entriesContainer.id = project.name
+        Display.title.textContent = project.name
     }
 
-
-    static updateInput() {
+    static updateInput () {
         Display.addProjectInput.addEventListener('keyup', (e) => {
             if (e.keyCode === 13) {
-                const some = document.querySelector(`.project[data-name="${e.target.value}"]`);
-                if (some) {return alert('Name of a project has to be unique.')};
+                const some = document.querySelector(`.project[data-name="${e.target.value}"]`)
+                if (some) { return alert('Name of a project has to be unique.') };
 
-                Events.emit('inputNewProject', e.target.value);
+                Events.emit('inputNewProject', e.target.value)
             }
         })
 
         Display.addEntryInput.addEventListener('keyup', (e) => {
             if (e.keyCode === 13) {
-                if (!Display.entriesContainer.id) return alert('You have to select project.');
-                const some = document.querySelector(`.entry[data-name="${e.target.value}"]`);
-                if (some) {return alert('Name of an entry has to be unique.')};
+                if (!Display.entriesContainer.id) return alert('You have to select project.')
+                const some = document.querySelector(`.entry[data-name="${e.target.value}"]`)
+                if (some) { return alert('Name of an entry has to be unique.') };
 
-                Events.emit('inputNewEntry', e.target.value, Display.entriesContainer.id);
+                Events.emit('inputNewEntry', e.target.value, Display.entriesContainer.id)
             }
         })
     }
 
-    
-    static updateEvents() {
-        Events.on('projectCreated', (project) => {Display.addProject(project)});
-        Events.on('entryCreated', (entry) => {Display.addEntry(entry)});
-        Events.on('projectRemoved', (name) => Display.removeProject(name));
-        Events.on('entryRemoved', (name) => Display.removeEntry(name));
+    static updateEvents () {
+        Events.on('projectCreated', (project) => { Display.addProject(project) })
+        Events.on('entryCreated', (entry) => { Display.addEntry(entry) })
+        Events.on('projectRemoved', (name) => Display.removeProject(name))
+        Events.on('entryRemoved', (name) => Display.removeEntry(name))
         Events.on('updateProject', (project) => Display.updateProject(project))
     }
 }
--- src/modules/project.js
+++ src/modules/project.js
@@ -1,49 +1,48 @@
-import Events from './events';
-import Entry from './entry';
-import isToday from 'date-fns/isToday';
+import Events from './events'
+import Entry from './entry'
 export default class Project {
-    constructor(name) {
-        this._name = name;
-        this.entries = [];
+    constructor (name) {
+        this._name = name
+        this.entries = []
         Events.on('inputNewEntry', (name, projectName) => {
-            if (projectName !== this.name) return;
-            this.add(new Entry(name, new Date(), false));
-        });
+            if (projectName !== this.name) return
+            this.add(new Entry(name, new Date(), false))
+        })
         Events.on('tryRemoveEntry', (name) => this.remove(name))
     }
 
-    set name(value) {
-        this._name = value;
+    set name (value) {
+        this._name = value
     }
 
-    get name() {
-        return this._name;
+    get name () {
+        return this._name
     }
 
-    setEntries(value) {
-        this.entries = value;
+    setEntries (value) {
+        this.entries = value
     }
 
-    getEntries() {
-        return this.entries;
+    getEntries () {
+        return this.entries
     }
 
-    get(entryName) {
-        return this.entries.find((entry) => entry.name === entryName);
+    get (entryName) {
+        return this.entries.find((entry) => entry.name === entryName)
     }
 
-    contains(entryName) {
-        return this.entries.some((entry) => entry.name === entryName);
-    }
-    
-    add(entry) {
-        if (this.contains(entry.name)) return;
-        this.entries.push(entry);
-        Events.emit('entryCreated', entry);
+    contains (entryName) {
+        return this.entries.some((entry) => entry.name === entryName)
     }
 
-    remove(entryName) {
-        this.entries = this.entries.filter((entry) => entry.name !== entryName);
-        Events.emit('entryRemoved', entryName);
+    add (entry) {
+        if (this.contains(entry.name)) return
+        this.entries.push(entry)
+        Events.emit('entryCreated', entry)
+    }
+
+    remove (entryName) {
+        this.entries = this.entries.filter((entry) => entry.name !== entryName)
+        Events.emit('entryRemoved', entryName)
     }
 }
