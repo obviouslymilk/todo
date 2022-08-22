8c6f6d9bf2aca26c686065116dc5af6760949545
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-08-22 10:33:57 +1000

    Change mod to prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
     entry: './src/index.js',
     devtool: 'inline-source-map',
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     }
 }
